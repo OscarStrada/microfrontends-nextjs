5f319b2741d07e14ab4a823b683b1b88caa1a1e1
Oscar Estrada <arturostrada2020@gmail.com>
2024-06-27 19:56:54 -0600

    chore(config): expose Button and setup cors headers

--- remote/next.config.mjs
+++ remote/next.config.mjs
@@ -3,16 +3,47 @@ import { NextFederationPlugin } from "@module-federation/nextjs-mf";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3000",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   webpack(config, options) {
     config.plugins.push(
       new NextFederationPlugin({
         name: "remote",
         filename: "static/chunks/remoteEntry.js",
-        remotes: {},
-        exposes: {},
-        shared: {},
+        exposes: {
+          "./Button": "./src/components/button.tsx",
+        },
+        extraOptions: {
+          exposePages: true,
+          enableImageLoaderFix: true,
+          enableUrlLoaderFix: true,
+        },
       })
     );
+
+    return config;
   },
 };
 
