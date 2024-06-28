0b6243d5ebee04a43e954eac41d9072f7886e3fa
Oscar Estrada <arturostrada2020@gmail.com>
2024-06-27 19:53:15 -0600

    chore(config): refactor next.config file for host app

--- host/next.config.mjs
+++ host/next.config.mjs
@@ -1,21 +1,34 @@
 import { NextFederationPlugin } from "@module-federation/nextjs-mf";
 
+const REMOTE_APP_URL =
+  process.env.NEXT_PUBLIC_REMOTE_APP_URL || "http://localhost:3001";
+
+const remotes = (isServer) => {
+  const location = isServer ? "ssr" : "chunks";
+
+  return {
+    remote: `remote@${REMOTE_APP_URL}/_next/static/${location}/remoteEntry.js`,
+  };
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config, options) {
+  webpack(config, { isServer }) {
     config.plugins.push(
       new NextFederationPlugin({
         name: "host",
         filename: "static/chunks/remoteEntry.js",
-        remotes: {
-          remote:
-            "remote@http://localhost:3001/_next/static/chunks/remoteEntry.js",
+        remotes: remotes(isServer),
+        extraOptions: {
+          exposePages: true,
+          enableImageLoaderFix: true,
+          enableUrlLoaderFix: true,
         },
-        exposes: {},
-        shared: {},
       })
     );
+
+    return config;
   },
 };
 
